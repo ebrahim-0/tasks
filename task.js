5ded638ae17e4aa6df856adef84f04e6bc8e61cb
Ebrahim Abd EL Razik <94077806+ebrahim-0@users.noreply.github.com>
2022-11-02 18:19:11 +0200

    Add files via upload

---
+++ task.js
@@ -0,0 +1,22 @@
+// function sum(num1, num2) {
+//   let result = 0;
+//   for (i = num1; i <= num2; i++) {
+//     result += i;
+//   }
+//   return result;
+// }
+
+// console.log(sum(2, 5));
+
+// ------------
+
+// let obj = {};
+
+// function count(string) {
+//   string.split("").forEach((ele) => {
+//     obj[ele] = obj[ele] ? obj[ele] + 1 : 1;
+//   });
+//   return obj;
+// }
+
+// console.log(count("hello"));
